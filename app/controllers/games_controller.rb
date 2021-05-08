3ba99d67179645a0c1ff6e0f5cab5946ff42c26a
Joanna Lisiak <joanna.lisiak13@gmail.com>
2021-05-08 15:52:29 +0200

    Display a set of random letters in the new page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,7 @@
 class GamesController < ApplicationController
   def new
+    alphabet = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+    @letters = alphabet.sample(10)
   end
 
   def score
